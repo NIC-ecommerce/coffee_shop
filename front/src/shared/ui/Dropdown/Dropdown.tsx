d82034cd153c5bf083e28ea263104671942cc4f1
Tuirbo <valerka130000@gmail.com>
2024-04-03 01:15:53 +0500

    Dropdown complete

--- front/src/shared/ui/Dropdown/Dropdown.tsx
+++ front/src/shared/ui/Dropdown/Dropdown.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import SmallArrow from "../Icons/SmallArrow/SmallArrow";
 
 interface DropdownProps {
   buttonStyle?: string;
@@ -25,13 +26,21 @@ const Dropdown: React.FC<DropdownProps> = ({
         {label}
       </button>
       {isOpen && (
-        <div className={`absolute top-full ${listStyle}`}>
+        <div className="relative">
+          <div className={`absolute top-full ${listStyle}`}>
           {options.map((option, index) => (
-            <div className="cursor-pointer" key={index}>
-              {option}
+            <div className={`cursor-pointer text-base pl-[5px] mt-[10px] pb-[10px] flex items-center 
+            ${ index === options.length - 1 ? "" : "border-b border-solid border-lightGray" }`}
+            key={index}>
+            {option}
+              <div className="absolute left-[290px]">
+                <SmallArrow/>
+              </div>
             </div>
-          ))}
+          ))}     
+          </div>
         </div>
+        
       )}
     </div>
   );
