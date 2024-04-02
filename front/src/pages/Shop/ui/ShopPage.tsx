37c73e5f5c56db8421abee3931ddb2f298edf079
Tuirbo <valerka130000@gmail.com>
2024-04-02 21:13:26 +0600

    catalog-update

--- front/src/pages/Shop/ui/ShopPage.tsx
+++ front/src/pages/Shop/ui/ShopPage.tsx
@@ -1,34 +1,15 @@
 import React from "react";
-import Card from "@/entities/Card/ui/Card";
 import Catalog from "@/features/Catalog/ui/Catalog";
+import CardList from "@/widgets/ui/CardList/CardList";
 
-let row_index =[1,2]
-let card_position_index = [1,2,3,4]
+
 
 export default function ShopPage() {
   return (
     <div>
       <div className="Shop__content pt-[60px] px-[90px] pb-[37px]">
         <Catalog/>
-         {
-            row_index.map(({},index) => (
-            <div className="flex gap-[7px] mb-[60px]"> {
-            card_position_index.map(({},index) => (
-              <div key = {index}>
-                <Card
-                image="/card/Latte.png"
-                name="Латте горячий "
-                price={1600}
-                description="Напиток на основе эспрессо и молока 
-                с небольшим количеством молочной 
-                пены." 
-                />
-              </div>
-            ))
-            }
-            </div>
-            ))
-          }        
+        <CardList/> 
       </div>
     </div>
   );
